6d7a67089442d09cd0cd2eb58fc1a1aa4a134fe7
YuanLee <liyuan520310@163.com>
2019-07-26 00:48:29 +0800

    v1

---
+++ Tree.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const afs = require('async-file')
+const fse = require('fs-extra')
+
+class Tree {
+    constructor(product, dir, date) {
+        console.log('Tree>args:', { product, dir, date })
+        date = new Date(date)
+        if (!date.getTime()) return console.error('\nERROR:::Date validated failed!')
+        this.product = product
+        this.dir = dir
+        this.dateStr = [ date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-')
+        // console.log('dateStr:', this.dateStr)
+        this.tree = []
+        this.info = {
+            folderCount: 0,
+            fileCount: 0,
+            lineCount: 0
+        }
+        return this.start()
+    }
+    async start() {
+        new Error('\nERROR:::Date validated failed!')
+        await this.read('', this.tree)
+        await this.save()
+        return this.tree
+    }
+    async read(dir, tree) {
+        console.log('read dir:', path.join(this.dir, dir))
+        const list = await afs.readdir(path.join(this.dir, dir))//.error(err => console.error(err))
+        // console.log('dir list:', list)
+        for (let name of list) {
+            const stat = await afs.stat(path.resolve(this.dir, dir, name))
+             // console.log('stat:', name, stat.isDirectory())
+            if (stat.isDirectory()) {
+                this.info.folderCount++
+                const child = { name, type: 'dir', children: [] }
+                tree.push(child)
+                await this.read(path.join(dir, name), child.children)
+                // console.log(child.children)
+            } else {
+                this.info.fileCount++
+                const lineCount = await this.lineCount(dir, name)
+                tree.push({ name, type: 'file', lineCount })
+                this.info.lineCount += lineCount
+            }
+        }
+    }
+    async lineCount(dir, name) {
+        const data = await afs.readFile(path.resolve(this.dir, dir, name), 'utf8')
+        return data.split(/[\n|\r]/).length
+    }
+    async save() {
+        const savePath = `./tmp/${this.product}/${this.dateStr}.json`
+        console.log('savePath:', savePath)
+        const savePathStat = await fse.ensureFile(savePath) // 如果文件不存在，则自动创建
+        console.log('savePathStat:', savePathStat)
+        await afs.writeFile(savePath, JSON.stringify({ info: this.info, tree: this.tree}, null ,2))
+        console.log(`\n:::[${this.product}]'s statistics data is stored in "${savePath}" at ${new Date().toLocaleString()}`)
+    }
+}
+
+module.exports = Tree
---
+++ generator.js
@@ -0,0 +1,42 @@
+const Tree = require('./Tree')
+const afs = require('async-file')
+const fse = require('fs-extra')
+
+class Generator {
+    constructor(product, dir, date = new Date()) {
+        date = new Date(date)
+        if (!date.getTime()) return console.error('\nERROR:::Date validated failed!')
+        this.product = product
+        this.dir = dir
+        this.date = date
+        this.startTime = new Date()
+        this.start()
+    }
+    async start() {
+        await new Tree(this.product, this.dir, this.date)
+        await this.record()
+    }
+    async record() {
+        await fse.ensureDir(`./tmp/${this.product}`) // 如果项目文件夹不存在，则自动创建
+        const dateStr = [this.date.getFullYear(), this.date.getMonth() + 1, this.date.getDate()].join('-')
+        const recordPath = `./tmp/${this.product}/record.json`
+        let record = await fse.pathExists(recordPath)
+        console.log('record pathExists:', record)
+        if (!record) record = []
+        else record = await fse.readJson(recordPath)
+        record = new Set(record)
+        record.add(dateStr)
+        record = Array.from(record).sort((a ,b) => { // 升序
+            a = new Date(a)
+            b = new Date(b)
+            return a.getTime() - b.getTime()
+        })
+        console.log('record:', record)
+        await afs.writeFile(recordPath, JSON.stringify(record, null, 2))
+    }
+}
+
+const [, , product, dir, date] = process.argv
+console.log('process.argv:', {product, dir, date})
+
+new Generator(product, dir, date)
