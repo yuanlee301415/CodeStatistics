a67970f93932d0cb9eb24c50385a38cf43b5a8a5
liyuan <liyuan520310@163.com>
2019-07-28 15:13:55 +0800

    Add:data

--- Tree.js
+++ Tree.js
@@ -5,10 +5,9 @@ const fse = require('fs-extra')
 class Tree {
     constructor(product, dir, date) {
         console.log('Tree>args:', { product, dir, date })
-
-        if (!product) return console.error('\nERROR:::Product name validated failed!')
+        if (!product) return this.error('Product name validated failed!')
         date = new Date(date)
-        if (!date.getTime()) return console.error('\nERROR:::Date validated failed!')
+        if (!date.getTime()) return this.error('Date validated failed!')
         this.product = product
         this.dir = dir
         this.dateStr = [ date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-')
@@ -23,8 +22,8 @@ class Tree {
     }
     async start() {
         const exists = await fse.pathExists(this.dir)
-        console.log('product path exists:', exists)
-        if (!exists) return console.error('\nERROR:::Product path validated failed!')
+        // console.log('product path exists:', exists)
+        if (!exists) return this.error('Product path validated failed!')
         await this.read('', this.tree)
         await this.save()
         return this.tree
@@ -35,15 +34,17 @@ class Tree {
         list = list.filter(name => ['assets', 'icons'].indexOf(name) === -1) // 过滤静态资源（图片/字体文件/图标）
         // console.log('dir list:', list)
         for (let name of list) {
-            const stat = await afs.stat(path.join(this.dir, dir, name))
+            const _path = path.join(this.dir, dir, name)
+            const stat = await afs.stat(_path)
             if (stat.isDirectory()) {
-                console.log('add name:', name)
+                console.log('+:', _path)
                 this.info.folderCount++
                 const child = { name, type: 'dir', children: [] }
                 tree.push(child)
                 await this.read(path.join(dir, name), child.children)
                 // console.log(child.children)
             } else {
+                console.log('-:', _path)
                 this.info.fileCount++
                 const lineCount = await this.lineCount(dir, name)
                 tree.push({ name, type: 'file', lineCount })
@@ -57,10 +58,14 @@ class Tree {
     }
     async save() {
         const savePath = `./tmp/${this.product}/${this.dateStr}.json`
-        console.log('savePath:', savePath)
+        // console.log('savePath:', savePath)
         await fse.ensureFile(savePath) // 如果文件不存在，则自动创建
         await afs.writeFile(savePath, JSON.stringify({ info: this.info, tree: this.tree}, null ,2))
-        console.log(`\n:::[${this.product}]'s statistics data is stored in "${savePath}" at ${new Date().toLocaleString()}`)
+        console.log(`\n[${this.product}]'s statistics data is stored in "${savePath}" at ${new Date().toLocaleString()}`)
+    }
+    error(msg) {
+        console.error(`\n\nERROR:::\n${msg}`)
+        return []
     }
 }
 
--- generator.js
+++ generator.js
@@ -5,34 +5,40 @@ const fse = require('fs-extra')
 class Generator {
     constructor(product, dir, date = new Date()) {
         date = new Date(date)
-        if (!date.getTime()) return console.error('\nERROR:::Date validated failed!')
+        if (!date.getTime()) return this.error('Date validated failed!')
         this.product = product
         this.dir = dir
         this.date = date
+        this.data = null // 统计结果
         this.startTime = new Date()
         this.start()
     }
     async start() {
-        await new Tree(this.product, this.dir, this.date)
+        this.data = await new Tree(this.product, this.dir, this.date)
         await this.record()
     }
     async record() {
+        // console.log('data:', this.data)
+        if (!this.data.length) return this.error('统计失败！！')
         await fse.ensureDir(`./tmp/${this.product}`) // 如果项目文件夹不存在，则自动创建
         const dateStr = [this.date.getFullYear(), this.date.getMonth() + 1, this.date.getDate()].join('-')
         const recordPath = `./tmp/${this.product}/record.json`
         let record = await fse.pathExists(recordPath) // 检测目录是否存在，不自动创建目录（Fix:ensureFile 自动创建的文件会生成一个空字符内容，导致当作JSON解析时报错）
-        console.log('record pathExists:', record)
+        // console.log('record pathExists:', record)
         if (!record) record = []
         else record = await fse.readJson(recordPath)
         record = new Set(record)
         record.add(dateStr)
-        record = Array.from(record).sort((a ,b) => { // 升序
-            a = new Date(a)
-            b = new Date(b)
-            return a.getTime() - b.getTime()
-        })
-        console.log('record:', record)
+        record = Array.from(record).sort((a, b) => new Date(a).getTime() - new Date(b).getTime()) // 升序
+        // console.log('record:', record)
         await afs.writeFile(recordPath, JSON.stringify(record, null, 2))
+        this.finish()
+    }
+    finish() {
+        console.log(`Done in ${Date.now() - this.startTime} ms.`)
+    }
+    error(msg) {
+        console.error(`\n\nERROR:::\n${msg}`)
     }
 }
 
